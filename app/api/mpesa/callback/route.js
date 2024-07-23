491089244a7919aa2347c4baca9656d8422a53ed
Technerdke <technerdke@gmail.com>
2024-07-23 08:37:09 -0500

    changed stk mpesa

--- app/api/mpesa/callback/route.js
+++ app/api/mpesa/callback/route.js
@@ -3,13 +3,15 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-    const { body } = await req.json();
-  
+  const { body } = await req.json();
+  try {
     // Handle the callback logic
-    console.log('M-Pesa Callback Received:', body);
-  
+    console.log("M-Pesa Callback Received:", body);
+
     // You can add logic here to verify the payment, log it, etc.
-  
-    return  NextResponse.json('Callback received', { status: 200 });
+
+    return NextResponse.json("Callback received", { status: 200 });
+  } catch (error) {
+    console.log(error);
   }
-  
+}
