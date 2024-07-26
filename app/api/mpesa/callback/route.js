cafaeef1561767325176df2708fe0bead0fe9ee9
Omar Maulid <98514692+Makavelisoftwares@users.noreply.github.com>
2024-07-26 09:11:43 +0300

    Update route.js

--- app/api/mpesa/callback/route.js
+++ app/api/mpesa/callback/route.js
@@ -2,7 +2,7 @@
 
 import { NextResponse } from "next/server";
 
-export async function GET(req) {
+export async function POST(req) {
   const body = await req.json();
   try {
     // Handle the callback logic
