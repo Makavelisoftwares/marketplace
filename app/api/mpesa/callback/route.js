2b210b97bf35e3e70b59453f642d98b4f8bde2ad
Omar Maulid <98514692+Makavelisoftwares@users.noreply.github.com>
2024-07-26 13:12:39 +0300

    Update route.js

--- app/api/mpesa/callback/route.js
+++ app/api/mpesa/callback/route.js
@@ -3,14 +3,19 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  const body = await req.json();
+  const {body}= await req.json();
   try {
     // Handle the callback logic
     console.log("M-Pesa Callback Received:", body);
 
-    // You can add logic here to verify the payment, log it, etc.
+if(body.stkCallback.ResultCode!=='0'){
+return NextResponse.json("error",{status:400})
+}
 
-    return NextResponse.json("Callback received", { status: 200 });
+    // You can add logic here to verify the payment, log it, etc
+const mpesametadata=body.stkCallback.CallbackMetadata
+
+    return NextResponse.json(mpesametadata, { status: 200 });
   } catch (error) {
     console.log(error);
 return NextResponse.json(error,{status:400})
